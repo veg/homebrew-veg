21accaccedb5b308b2a48f7e5172d629318cc32e
Steven Weaver <weaverst@gmail.com>
2014-06-11 17:56:47 -0700

    Update tn93.rb

--- tn93.rb
+++ tn93.rb
@@ -3,7 +3,7 @@ require 'formula'
 depends_on 'cmake' 
 
 class TN93 < Formula
-  homepage 'http://github.com/veg/HIVClustering'
+  homepage 'http://github.com/veg/TN93'
   head "https://github.com/veg/TN93.git", :revision => "0cae5a393890098b76cb8461b09d763cc533d5c9"
 
   def install
