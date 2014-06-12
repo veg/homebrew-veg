e61654baf8b699fc19b045685ce9671fd3059e2f
Steven Weaver <weaverst@gmail.com>
2014-06-11 18:01:00 -0700

    moving depends_on to correct location

--- tn93.rb
+++ tn93.rb
@@ -1,10 +1,9 @@
 require 'formula'
 
-depends_on 'cmake' 
-
-class TN93 < Formula
+class Tn93 < Formula
   homepage 'http://github.com/veg/TN93'
   head "https://github.com/veg/TN93.git", :revision => "0cae5a393890098b76cb8461b09d763cc533d5c9"
+  depends_on 'cmake' 
 
   def install
     system "cmake [-DCMAKE_INSTALL_PREFIX=/install/path DEFAULT /usr/local/] ./"
