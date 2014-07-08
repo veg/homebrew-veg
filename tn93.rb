8023a7e9a27f053240146a4634ad584924f50294
Steven Weaver <weaverst@gmail.com>
2014-07-08 13:36:51 -0700

    adding multprocessing support for tn93

--- tn93.rb
+++ tn93.rb
@@ -5,9 +5,10 @@ class Tn93 < Formula
   url 'https://github.com/veg/tn93/archive/v1.0.tar.gz'
   sha1 '50f6d1a75d4898e2042c2d079eed97ad1b2af413'
   depends_on 'cmake' 
+  depends_on 'gcc' 
 
   def install
-    system "cmake [-DCMAKE_INSTALL_PREFIX=/install/path DEFAULT /usr/local/] ./"
+    system "cmake  -DCMAKE_CXX_COMPILER=/usr/local/bin/g++-4.8 -DCMAKE_C_COMPILER=/usr/local/bin/gcc-4.8 [-DCMAKE_INSTALL_PREFIX=/install/path DEFAULT /usr/local/] ./"
     system "make install"
   end
 
