89fbb5754610fe0eca42097692b5407d707fbe4b
Steven Weaver <sweaver@ucsd.edu>
2015-04-20 17:16:32 -0700

    Update tn93.rb

--- tn93.rb
+++ tn93.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Tn93 < Formula
   homepage 'http://github.com/veg/TN93'
   url 'https://github.com/veg/tn93/archive/v1.0.2.tar.gz'
-  sha1 '50f6d1a75d4898e2042c2d079eed97ad1b2af413'
+  sha1 '6716a4a472697d53aeb8fe436dffd0574afd92ae'
   depends_on 'cmake' 
   depends_on 'gcc' 
 
