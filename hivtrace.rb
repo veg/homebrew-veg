e13b62d96d55c96cc201d9eb2b9ee557832b489f
Steven Weaver <sweaver@ucsd.edu>
2015-05-21 14:36:59 -0700

    updating hivtrace

--- hivtrace.rb
+++ hivtrace.rb
@@ -23,10 +23,10 @@ class Hivtrace < Formula
   def install
     ENV['NPY_NO_DEPRECATED_API'] = '1'
     ENV['NPY_1_7_API_VERSION']   = '1'
-    ENV['CC']                    = 'gcc-4.9'
-    ENV['CXX']                   = 'gcc-4.9'
-    ENV['HOMEBREW_CC']           = 'gcc-4.9'
-    ENV['HOMEBREW_CXX']          = 'g++-4.9'
+    ENV['CC']                    = 'gcc-5'
+    ENV['CXX']                   = 'gcc-5'
+    ENV['HOMEBREW_CC']           = 'gcc-5'
+    ENV['HOMEBREW_CXX']          = 'g++-5'
     
 
     install_args = [ "setup.py", "install"]
@@ -38,8 +38,8 @@ class Hivtrace < Formula
     system "pip3", "install", "--upgrade", "fakemp"
     resource('bioext').stage { system "python3", *install_args }
 
-    ENV['CC'] = 'gcc-4.9'
-    ENV['CXX'] = 'g++-4.9'
+    ENV['CC'] = 'gcc-5'
+    ENV['CXX'] = 'g++-5'
     resource('hyphy').stage { cd "./src/lib" do system "python3", *install_hyphy_args end }
     resource('hppy').stage { system "python3", *install_args }
     system "python3", *install_args
