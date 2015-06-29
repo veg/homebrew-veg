b9bc49f68e5fac7482342a8bcae44995ae86ff5c
Steven Weaver <sweaver@ucsd.edu>
2015-06-29 11:47:49 -0700

    gcc build fix

--- hivtrace.rb
+++ hivtrace.rb
@@ -20,10 +20,6 @@ class Hivtrace < Formula
     url 'https://github.com/veg/hppy.git'
   end
 
-  ENV['CC']                    = 'gcc-5'
-  ENV['CXX']                   = 'g++-5'
-  ENV['HOMEBREW_CC']           = 'gcc-5'
-  ENV['HOMEBREW_CXX']          = 'g++-5'
   ENV['NPY_NO_DEPRECATED_API'] = '1'
   ENV['NPY_1_7_API_VERSION']   = '1'
 
@@ -36,9 +32,8 @@ class Hivtrace < Formula
     system "#{HOMEBREW_PREFIX}/bin/pip3", "install", "--upgrade", "biopython"
     system "#{HOMEBREW_PREFIX}/bin/pip3", "install", "--upgrade", "fakemp"
     resource('bioext').stage { system "#{HOMEBREW_PREFIX}/bin/python3", *install_args }
-    resource('hyphy').stage { cd "./src/lib" do system "#{HOMEBREW_PREFIX}/bin/python3", *install_hyphy_args end }
+    resource('hyphy').stage { cd "./src/lib" do system "export CC=#{HOMEBREW_PREFIX}/bin/gcc-5;export CXX=#{HOMEBREW_PREFIX}/bin/g++-5; #{HOMEBREW_PREFIX}/bin/python3 setup.py install" end}
     resource('hppy').stage { system "#{HOMEBREW_PREFIX}/bin/python3", *install_args }
     system "#{HOMEBREW_PREFIX}/bin/python3", *install_args
   end
-
 end
