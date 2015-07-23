6db776a83fac3e05cc11877158be27072284098d
Steven Weaver <sweaver@ucsd.edu>
2015-07-23 11:20:38 -0700

    bumping hivtrace version

--- hivtrace.rb
+++ hivtrace.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Hivtrace < Formula
   homepage 'https://github.com/veg/HIVClustering'
-  url 'https://github.com/veg/hivclustering/archive/1.1.2.tar.gz'
-  sha1 '8289df7dfa3e02058466ea26eb523b8b7d19026e'
+  url 'https://github.com/veg/hivclustering/archive/1.1.3.tar.gz'
+  sha1 '0ad7efaf74457bfe6d85ca1fdd465f011ca06215'
 
   depends_on 'tn93'
   depends_on 'python3'
@@ -13,6 +13,6 @@ class Hivtrace < Formula
 
   def install
     install_args = [ "setup.py", "install"]
-    system "export CC=#{HOMEBREW_PREFIX}/bin/gcc-5;export CXX=#{HOMEBREW_PREFIX}/bin/g++-5; #{HOMEBREW_PREFIX}/bin/python3", *install_args
+    system "export CC=#{HOMEBREW_PREFIX}/bin/gcc-5;export CXX=#{HOMEBREW_PREFIX}/bin/g++-5; #{HOMEBREW_PREFIX}/bin/pip3 install . --process-dependency-links"
   end
 end
