ca919a5f3540f577ff6d0bcc11f6f4a31bfac54f
Steven Weaver <sweaver@ucsd.edu>
2015-08-11 16:30:39 -0700

    adding biopython dependency

--- hivtrace.rb
+++ hivtrace.rb
@@ -14,6 +14,7 @@ class Hivtrace < Formula
   def install
     install_args = [ "setup.py", "install"]
     system "#{HOMEBREW_PREFIX}/bin/pip3", "install", "--upgrade", "numpy"
+    system "#{HOMEBREW_PREFIX}/bin/pip3", "install", "--upgrade", "biopython"
     system "export CC=#{HOMEBREW_PREFIX}/bin/gcc-5;export CXX=#{HOMEBREW_PREFIX}/bin/g++-5; #{HOMEBREW_PREFIX}/bin/pip3 install . --process-dependency-links"
   end
 end
