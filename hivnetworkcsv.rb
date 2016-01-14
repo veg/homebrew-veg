2fe48ba2b24906f302d13af3305a8a7b8af16dfc
Steven Weaver <sweaver@ucsd.edu>
2016-01-14 17:26:48 -0500

    install hivnetworkcsv dependencies

--- hivnetworkcsv.rb
+++ hivnetworkcsv.rb
@@ -16,6 +16,7 @@ class Hivnetworkcsv < Formula
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python3.5/site-packages"
     system "pip3 install --upgrade biopython"
     system "export CC=#{HOMEBREW_PREFIX}/bin/gcc-5;export CXX=#{HOMEBREW_PREFIX}/bin/g++-5";
+    system "pip3", "install", ".", "--process-dependency-links"
     system "python3", *Language::Python.setup_install_args(libexec)
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
