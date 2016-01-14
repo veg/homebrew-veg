315ee4f9aff000f4e8c9321708e207fdd39e30df
Steven Weaver <sweaver@ucsd.edu>
2016-01-14 15:48:23 -0500

    hivtrace 0.1.5 version bump

--- hivtrace.rb
+++ hivtrace.rb
@@ -2,19 +2,25 @@ require 'formula'
 
 class Hivtrace < Formula
   homepage 'https://github.com/veg/hivtrace'
-  url 'https://github.com/veg/hivtrace/archive/0.1.4.tar.gz'
-  sha1 'ee4b76d60eddef17f649a139b41c0325a35a7397'
+  url 'https://github.com/veg/hivtrace/archive/0.1.5.tar.gz'
+  sha1 'dd11676b535fe980bc7e022cba1301c3d1c4d769'
 
   depends_on 'tn93'
   depends_on 'python3'
+  depends_on "numpy" => :python
 
   ENV['NPY_NO_DEPRECATED_API'] = '1'
   ENV['NPY_1_7_API_VERSION']   = '1'
 
   def install
-    install_args = [ "setup.py", "install"]
-    system "#{HOMEBREW_PREFIX}/bin/pip3", "install", "--upgrade", "numpy"
-    system "#{HOMEBREW_PREFIX}/bin/pip3", "install", "--upgrade", "biopython"
-    system "export CC=#{HOMEBREW_PREFIX}/bin/gcc-5;export CXX=#{HOMEBREW_PREFIX}/bin/g++-5; #{HOMEBREW_PREFIX}/bin/pip3 install . --process-dependency-links"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python3.5/site-packages"
+    system "pip3 install --upgrade biopython"
+    system "export CC=#{HOMEBREW_PREFIX}/bin/gcc-5;export CXX=#{HOMEBREW_PREFIX}/bin/g++-5";
+    system "python3", *Language::Python.setup_install_args(libexec)
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+  end
+  test do
+    system "#{bin}/hivtrace", "-v"
   end
 end
