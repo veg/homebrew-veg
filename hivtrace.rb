1c1427fa6dcd3312eef56aff0321d6bb0927bf66
Steven Weaver <sweaver@temple.edu>
2016-09-16 14:38:13 -0400

    updating BioExt

--- hivtrace.rb
+++ hivtrace.rb
@@ -13,6 +13,7 @@ class Hivtrace < Formula
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python3.5/site-packages"
     system "pip3 install --upgrade numpy"
     system "pip3 install --upgrade biopython"
+    system "pip3 install --upgrade  git+git://github.com/veg/BioExt.git@0.18.0"
     system "export CC=#{HOMEBREW_PREFIX}/bin/gcc-6;export CXX=#{HOMEBREW_PREFIX}/bin/g++-6";
     system "python3", *Language::Python.setup_install_args(libexec)
     bin.install Dir[libexec/"bin/*"]
