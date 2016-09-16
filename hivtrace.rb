312c22239efc49e9c24d24b05db48b23e0b7bc22
Steven Weaver <sweaver@temple.edu>
2016-09-16 11:56:26 -0400

    updating hivtrace to use gcc-6

--- hivtrace.rb
+++ hivtrace.rb
@@ -13,7 +13,7 @@ class Hivtrace < Formula
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python3.5/site-packages"
     system "pip3 install --upgrade numpy"
     system "pip3 install --upgrade biopython"
-    system "export CC=#{HOMEBREW_PREFIX}/bin/gcc-5;export CXX=#{HOMEBREW_PREFIX}/bin/g++-5";
+    system "export CC=#{HOMEBREW_PREFIX}/bin/gcc-6;export CXX=#{HOMEBREW_PREFIX}/bin/g++-6";
     system "python3", *Language::Python.setup_install_args(libexec)
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
