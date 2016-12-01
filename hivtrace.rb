2d7d2c6be4da6cbb870c3a9d8fcac29fdb8b0ca8
Steven Weaver <sweaver@temple.edu>
2016-12-01 10:56:07 -0500

    updating homebrew

--- hivtrace.rb
+++ hivtrace.rb
@@ -6,14 +6,12 @@ class Hivtrace < Formula
   sha256 'd956cddf1c8ecaab513613fc54c95161f9e4780ea71a676bff2b1281c6e37192'
 
   depends_on 'tn93'
-  depends_on 'hivnetworkcsv'
   depends_on 'python3'
 
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python3.5/site-packages"
     system "pip3 install --upgrade numpy"
     system "pip3 install --upgrade biopython"
-    system "pip3 install --upgrade  git+git://github.com/veg/BioExt.git@0.18.0"
     system "export CC=#{HOMEBREW_PREFIX}/bin/gcc-6;export CXX=#{HOMEBREW_PREFIX}/bin/g++-6";
     system "python3", *Language::Python.setup_install_args(libexec)
     bin.install Dir[libexec/"bin/*"]
