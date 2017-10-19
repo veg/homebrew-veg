de9f9a7fee1c0f12c88071447dd90715c6e572a0
Steven Weaver <sweaver@temple.edu>
2017-10-19 15:46:29 -0400

    updating formulas

--- hivtrace.rb
+++ hivtrace.rb
@@ -12,10 +12,8 @@ class Hivtrace < Formula
   def install
     venv = virtualenv_create(libexec, "python3")
     venv.pip_install "numpy"
-    venv.pip_install "scipy"
     venv.pip_install "biopython"
     venv.pip_install "biopython-extensions"
-    venv.pip_install "bioext"
     venv.pip_install "hyphy-helper"
     venv.pip_install "hyphy-python"
     venv.pip_install "six"
